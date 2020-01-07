c73239c7d826080392815758d3ff51dd2b5c1ea4
Harith.Fanani <harith.fanani@traveloka.com>
2020-01-07 14:09:46 +0700

    remove unused file

--- testrailAccount.py
+++
@@ -1,7 +0,0 @@
-from testrail import *
-
-client = APIClient('https://traveloka.testrail.net/')
-
-#insert your testrail credential
-client.user = 'sdet@traveloka.com'
-client.password = 'Automation1234567890'
